5f7288ee814e39320de50e46ec1edce50e800758
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2021-05-07 11:41:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var grandma, girl_running;
-var banana ,bananaImage, obstacle, obstacleImage;
+var goldCoin ,goldCoinImage, obstacle, obstacleImage;
 var FoodGroup, obstacleGroup;
 var ground;
 var background1,backgroundImage;
@@ -14,26 +14,26 @@ var life;
 function preload(){
 girl_running =  loadImage("eagle.gif")
   
-  girl_collided = loadAnimation("sprite_7.png");
+  girl_collided = loadAnimation("gameover.png");
   
-  bananaImage = loadImage("banana.png");
+  goldCoinImage = loadImage("Gold coin.png");
   obstacleImage = loadImage("obstacle.png");
   backgroundImage = loadImage("jungle.jpg");
 }
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(500,600);
   
-  girl = createSprite(80,500,50,50);
+  girl = createSprite(80,500,70,70);
   girl.addImage("girl",girl_running);
   girl.addAnimation("collided",girl_collided);
-  girl.scale = 0.2;
+  girl.scale = 0.4;
   
-  ground = createSprite(250,575,1200,20);
+  ground = createSprite(250,575,500,20);
   ground.velocityX = -3;
   ground.x = ground.width /2;
   
-  background1 = createSprite(700,300,1500,1500);
+  background1 = createSprite(250,300,800,1500);
   background1.addImage("background",backgroundImage);
   background1.x = background1.width /2;
  
@@ -56,32 +56,17 @@ function draw() {
   if(gameState === PLAY){
     background1.velocityX = -3;
     
-  if (ground.x < 0){
+  if (background1.x < 0){
+      background1.x = background1.width/2;
+    }
+
+    if (ground.x < 0){
       ground.x = ground.width/2;
     }
   
-    if (keyDown("e")) {
-       image(girl_running,50,50);
-    }
   
-    if(keyDown("space") && girl.y >= 500&&girl.scale ==0.2){
-      girl.velocityY = -12;
-    }
-  
-  if(keyDown("space") && girl.y >= 470&&girl.scale ==0.3){
-      girl.velocityY = -12;
-    }
-  
-  if(keyDown("space") && girl.y >= 438&&girl.scale ==0.4){
-      girl.velocityY = -12;
-    }
-  
-  if(keyDown("space") && girl.y >= 408&&girl.scale ==0.5){
-      girl.velocityY = -12;
-    }
-  
-   if(keyDown("space") && girl.y >= 377&&girl.scale ==0.6){
-      girl.velocityY = -12;
+   if(keyDown("space") && girl.y >= 377){
+      girl.velocityY = -8;
     }
   
   if (background1.x<0){
@@ -90,12 +75,17 @@ function draw() {
    }    
   
   if (FoodGroup.isTouching(girl)){
+    if(girl.scale<0.4) {
+      girl.scale+=0.1;
+    }
+    
     FoodGroup.destroyEach();
     score = score+2;
     }
   
   if (obstacleGroup.isTouching(girl)){
       girl.scale = 0.2;
+      girl.x = 200;
       life = life-1;
       obstacleGroup.destroyEach();
   }
@@ -146,25 +136,26 @@ function draw() {
 
 function spawnFood(){
   if (frameCount%180 == 0){
-    banana = createSprite(600,200,40,40);
-    banana.addImage("banana",bananaImage);
+    banana = createSprite(600,200,55,55);
+    banana.addImage("banana",goldCoinImage);
     banana.scale = 0.1;
     banana.velocityX = - 3;
     banana.lifetime = 400/3;
     
-    rand = Math.round(random(250,380));
+    rand = Math.round(random(200,380));
     banana.y = rand;
+    banana.x = rand;
     
     FoodGroup.add(banana);
   }
 }
 
 function spawnObstacles(){
-  if (frameCount%300 == 0){
-    obstacles = createSprite(600,530,50,50);
+  if (frameCount%235 == 0){
+    obstacles = createSprite(600,450,35,35);
     obstacles.addImage ("obtacles",obstacleImage);
     obstacles.lifetime = 400;
-    obstacles.scale = 0.2;
+    obstacles.scale = 0.15;
     obstacles.velocityX = -3;
     obstacleGroup.add(obstacles);
   }
